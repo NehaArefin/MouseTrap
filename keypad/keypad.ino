f557483f3065c83e37e21458577942d7d4fbcdde
Garry Sandhu <Singh39g@uregina.ca>
2016-12-05 23:23:18 -0600

    fixed bugs

--- keypad/keypad.ino
+++ keypad/keypad.ino
@@ -1,40 +1,4 @@
-//// Include the required Wire library for I2C<br>
-//#include <Wire.h>
-//int LED = 5;
-//int x = 0;
-//void setup() {
-//  // Define the LED pin as Output
-//  pinMode (LED, OUTPUT);
-//  // Start the I2C Bus as Slave on address 9
-//  Wire.begin(8); 
-//  // Attach a function to trigger when something is received.
-//  Wire.onReceive(receiveEvent);
-//  Serial.begin(9600);
-//}
-//void receiveEvent(int bytes) {
-//  x = Wire.read();    // read one character from the I2C
-//}
-//void loop() {
-//  //If value received is 0 blink LED for 200 ms
-//  if (x == '0') {
-//    digitalWrite(LED, HIGH);
-//    delay(200);
-//    digitalWrite(LED, LOW);
-//    Serial.println(x);
-//    delay(200);
-//  }
-//   Serial.println(x);
-//  //If value received is 3 blink LED for 400 ms
-//  if (x == '3') {
-//    digitalWrite(LED, HIGH);
-//    delay(400);
-//    digitalWrite(LED, LOW);
-//     Serial.println(x);
-//    delay(400);
-//  }
-//}
-/*4x4 Matrix Keypad connected to Arduino
-This code prints the key pressed on the keypad to the serial port*/
+
 
 #include <Keypad.h>
 #include <Servo.h>
